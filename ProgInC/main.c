20f9b0109292f68a7e76b845b3233ef55f106206
TimoVanBeelen <timovbeelen@hotmail.com>
2020-11-17 14:10:33 +0100

    Take 2 on first assignment

--- ProgInC/main.c
+++ ProgInC/main.c
@@ -7,19 +7,19 @@
 #include <stdio.h>
 
 int main() {
-    // Variables
+    /* Variables */
     double tempC, tempK, tempF;
 
-    // Receive input for a temperature
+    /* Receive input for a temperature */
     printf("Give a temperature to convert\n");
     scanf("%lf", &tempC);
 
-    // Calculate the temperatures
+    /* Calculate the temperatures */
     tempK = tempC + 273.15;
     tempF = 1.8*tempC + 32;
 
-    // Give the output
-    printf("C \t\tK  \t\tF\n"
-           "%.2f \t%.2f \t%.2f", tempC, tempK, tempF);
+    /* Give the output */
+    printf("C \tK  \tF\n"
+           "%.2f \t%.2f \t%.2f\n", tempC, tempK, tempF);
     return 0;
 }
